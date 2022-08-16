19e0c1ef2c217b46fe0806be96694349eab08181
chaojunyang <chaojunyang@yunify.com>
2022-08-17 00:29:46 +0800

    fix: 更改后端项目文件配置

--- shopping-java/feign-api/src/main/java/com/ycj/feign/clients/UserClient.java
+++ shopping-java/feign-api/src/main/java/com/ycj/feign/clients/UserClient.java
@@ -1,6 +1,6 @@
 package com.ycj.feign.clients;
 
-import com.ycj.user.pojo.User;
+import com.ycj.feign.pojo.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
